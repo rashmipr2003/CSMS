38e9de4034c179188705144d8c92feb895dc37bc
Rashmi P R <rashmipr9496@gmail.com>
2025-01-17 11:44:53 +0530

    Updated Main.java file

--- src/main/java/com/cars24/runner/Main.java
+++ src/main/java/com/cars24/runner/Main.java
@@ -14,6 +14,7 @@ import java.sql.SQLException;
 import java.util.Scanner;
 
 import static com.cars24.util.DbUtil.getDbconnection;
+import static com.cars24.util.DbUtil.closeConnection;
 
 
 public class Main {
@@ -42,7 +43,10 @@ public class Main {
                     break;
                 case 0:
                     exit=false;
+
             }
         }
+        scanner.close();
+        closeConnection();
     }
 }
